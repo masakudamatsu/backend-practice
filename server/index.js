830fbf2620746db77445188a4be24f6798c688f5
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-03-18 14:01:58 +0900

    feat(server/index.js): enable CORS
    allow access to the server from a different server
    
    close #2

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 4000;
+
+app.use(cors);
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
